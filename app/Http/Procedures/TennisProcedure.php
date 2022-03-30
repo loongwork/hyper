38dc537320205a093ce274742ed0edc6d04d0a9c
sunxyw <xy2496419818@gmail.com>
2022-03-30 17:41:41 +0800

    feat(rpc-tennis): add ping method

---
+++ app/Http/Procedures/TennisProcedure.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Procedures;
+
+use Illuminate\Http\Request;
+use Sajya\Server\Procedure;
+
+class TennisProcedure extends Procedure
+{
+    /**
+     * The name of the procedure that will be
+     * displayed and taken into account in the search
+     *
+     * @var string
+     */
+    public static string $name = 'tennis';
+
+    /**
+     * Execute the procedure.
+     *
+     * @return string
+     */
+    public function ping(): string
+    {
+        return 'pong';
+    }
+}
